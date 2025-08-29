0e7452189c9b9722aa495adf0437346f66d39f93
Lalit Sharma <136457354+lalitsharma0@users.noreply.github.com>
2025-08-30 00:20:45 +0530

    Create task1.py

---
+++ task1.py
@@ -0,0 +1,16 @@
+
+a1 = float(input("Enter the first number: "))
+a2 = float(input("Enter the second number: "))
+
+
+addition = a1 + a2
+subtraction = a1 - a2
+multiplication = a1 * a2
+division = a1 / a2 
+
+
+print("\nResults of Mathematical Operations:")
+print(f"Addition: {addition}")
+print(f"Subtraction: {subtraction}")
+print(f"Multiplication: {multiplication}")
+print(f"Division: {division}")
