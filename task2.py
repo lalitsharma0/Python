b16e3fe669991ca35b7e6c530f89401bdc651df7
Lalit Sharma <136457354+lalitsharma0@users.noreply.github.com>
2025-08-31 21:06:07 +0530

    Delete task2.py

--- task2.py
+++
@@ -1,9 +0,0 @@
-
-first_name = input("Enter your first name: ")
-last_name = input("Enter your last name: ")
-
-
-full_name = first_name + " " + last_name
-
-
-print(f"\nHello, {full_name}! Welcome to Python programming.")
